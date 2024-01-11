ee0cc4b75c1e64400c72cd30a432085021f6e2f1
Jonah <82569523+Jonah987654321@users.noreply.github.com>
2024-01-11 13:12:08 +0100

    Create CSVParser.java

---
+++ CSVParser.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVParser {
+    private String filePath;
+
+    public CSVParser(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<String[]> parse() {
+        List<String[]> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                data.add(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+}
